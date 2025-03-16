e9f4c193256744fe356b7122c6daf2443b7ac83c
palaci spaci <sirdifino@gmail.com>
2025-03-16 20:52:35 +0100

    added top-rated

--- src/app/top-rated/top-rated.component.ts
+++ src/app/top-rated/top-rated.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../movie.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-top-rated',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './top-rated.component.html',
-  styleUrl: './top-rated.component.css'
+  styleUrl: './top-rated.component.css',
 })
-export class TopRatedComponent {
+export class TopRatedComponent implements OnInit {
+  topRatedMovies: any[] = [];
+  errorMessage: string | null = null;
+  title = 'Top Rated';
 
+  constructor(private movieService: MovieService) {}
+
+  ngOnInit(): void {
+    this.fetchTopRatedMovies();
+  }
+
+  fetchTopRatedMovies(): void {
+    this.movieService.getTopRatedMovies().subscribe(
+      (data) => {
+        this.topRatedMovies = data.results;
+        this.errorMessage = null;
+      },
+      (error) => {
+        console.error('API Error:', error);
+        this.errorMessage =
+          'Failed to load top-rated movies. Please try again later.';
+      }
+    );
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiKey: 'acd9f928cbd7f0a07860670377c1fad4',
+};
